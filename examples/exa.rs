5f178fc7ca30f6121820714a56abbd8b008ef646
softtiny <yingxiancapatin@gmail.com>
2023-05-25 11:44:18 +0800

    boot ws

---
+++ examples/exa.rs
@@ -0,0 +1,18 @@
+use std::{
+    collections::HashMap,
+    io::Error as IoError,
+    net::SocketAddr,
+    sync::{Arc, Mutex},
+};
+
+use futures_channel::mpsc::{unbounded, UnboundedSender};
+use futures_util::{future, pin_mut, stream::TryStreamExt, StreamExt};
+
+use tokio::net::{TcpListener, TcpStream};
+use tokio_tungstenite::connect_async;
+use tokio_tungstenite::tungstenite::protocol::Message;
+
+fn main(){
+
+}
+
